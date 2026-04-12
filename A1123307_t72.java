69d040883d7d50a1cda016f1693153a76a330d81
warren1008 <A1123307@mail.nuk.edu.tw>
2026-04-12 21:43:50 +0800

    update t72 file

--- A1123307_t72.java
+++ A1123307_t72.java
@@ -5,20 +5,27 @@ public class A1123307_t72 {
             {77, 33, 68, 45, 23},
             {43, 55, 43, 67, 65}
         };
+        
         char product[] = {'A', 'B', 'C', 'D', 'E'};
+        double[] averageResult = average(sales);
+
         for(int i =0; i < product.length; i++){
-            System.out.printf("The average sales for product[%c] = %.2f\n", product[i], average(sales[][i]));
+            System.out.printf("The average sales for product%c = %.2f\n", product[i], averageResult[i]);
         } 
 
     }
-    public static float average(int arr[][]){
+    public static double[] average(int arr[][]){
+        double[] averageResult = new double[arr[0].length];
+        
         for(int c = 0; c < arr[0].length; c++){
-            int sum = 0;
-            
+            double sum = 0;
+    
             for(int r = 0; r < arr.length; r++){
                 sum += arr[r][c];
             }
-            return sum;
+            averageResult[c] = sum / arr.length;
         }
+        return averageResult;
+
     }
 }
